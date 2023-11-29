93f8ab84253fdde929b61d289826cd22e9f2385e
juliano silva <juliano.sil6@gmail.com>
2023-11-29 19:12:38 -0300

    ajustes flowbite

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,18 +1,20 @@
-import { Outlet, Link, NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
 
   return (
     <>
-    <main>
-        <h1 className="mainHeader">LibraryGuru</h1>
-        <section id="login">
-            <div id="imagem"></div>
-            <div id="formulario">
-            <Outlet />
+        <main className="container mx-auto content-center">
+            <h2 className="text-4xl font-extrabold dark:text-white text-center mb-16">LibraryGuru</h2>
+            <div className="flex justify-center">
+            <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl">
+                <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/img/pexels-pixabay-159711.jpg" alt="" />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                <Outlet />
+                </div>
             </div>
-        </section>
-    </main>
+            </div>
+        </main>
     </>
   );
 }
